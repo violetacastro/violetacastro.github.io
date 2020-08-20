a22252a15152ed4dd0eebc182b05a7c26955dc1c
root <violetacastrogutierrez@gmail.com>
2020-08-20 20:36:34 +0000

    add submit functionallity to the form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React, {Component} from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
   state = {
     characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
     ],
   }
 
@@ -29,8 +14,12 @@ class App extends Component {
     this.setState({
       characters: characters.filter((character, i) => {
         return i !== index
-      }),
-    })
+      })
+    });
+  }
+
+  handleSubmit = character => {
+    this.setState({characters: [...this.state.characters, character]});
   }
 
   render() {
@@ -39,6 +28,7 @@ class App extends Component {
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     )
   }
